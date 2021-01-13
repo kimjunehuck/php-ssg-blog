f681065a88b8d01e4ec4a48bd7180ca4bdc337a9
kimjunehuck <72539587+kimjunehuck@users.noreply.github.com>
2021-01-13 17:09:58 +0900

    베포3

--- common.js
+++ common.js
@@ -89,3 +89,19 @@ EditorViewer__init();
 
 /* 토스트 UI 끝 */
 
+/* 마우스 모양 */
+const $cursor = $('.cursor');
+const $cursorSahdow = $('.cursor-shadow');
+
+$(window).mousemove(function(e) {
+  $cursor.css({
+    top:e.clientY,
+    left:e.clientX
+  });
+  
+  $cursorSahdow.css({
+    top:e.clientY,
+    left:e.clientX
+  });
+});
+/* 마우스 끝 */
