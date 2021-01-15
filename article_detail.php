add79a5ce04c72937efa3575b7266784b0f9259b
kimjunehuck <72539587+kimjunehuck@users.noreply.github.com>
2021-01-16 01:33:53 +0900

    한번더

--- article_detail.php
+++ article_detail.php
@@ -14,6 +14,24 @@
 
 <section class="section-article-detail con-min-width">
     <div class="con">
+        <h1 class="article-list-box__title"><a href="article_detail_1.ssghtml.php"><?=$selectedArticle["title"]?></a></h1>
+
+        <div class="article-list-box__reg-date"><?=$selectedArticle["regDate"]?></div>
+        
+        <div class="article-list-box__writer">
+            <span><?=$selectedArticle["writerName"]?></span>
+            <span><?=$selectedArticle["writerAvatar"]?></span>
+        </div>
+        
+        <div class="article-list-box__body">
+            <script type="text/x-template"><?=$selectedArticle['body']?></script>
+            <div class="toast-ui-viewer"></div>
+        </div>
+    </div>
+</section>
+
+<section class="section-article-detail con-min-width">
+    <div class="con">
         <h1 class="article-list-box__title"><a href="article_detail_2.ssghtml.php"><?=$selectedArticle["title"]?></a></h1>
 
         <div class="article-list-box__reg-date"><?=$selectedArticle["regDate"]?></div>
