34709063189859669178cf319240064e1afeb725
kimjunehuck <72539587+kimjunehuck@users.noreply.github.com>
2021-02-24 18:11:52 +0900

    pf

--- pf.ssghtml.php
+++ pf.ssghtml.php
@@ -14,8 +14,31 @@ require_once "head.php";
     <span>
       PORFOLIO
     </span>
+
+    <div class="mobile-porfolio">
+      <div class="img-box">
+        <img src="https://cdn.jsdelivr.net/gh/kimjunehuck/php-ssg-blog/img/moblie/Puple.png" alt="">
+      </div>
+      <div class="mobile-text">
+        <span>Purple Music APP</span>
+        <span>
+          퇴근 할때 심심할 때 
+          <br>
+          음악 들을래요? 사용자가 편하게 사용할 수 있는
+          <br>
+          음악 어플입니다.
+        </span>
+      <div class="view">
+        <span>
+          <a href="https://b.tistory.xn--3e0b707e/pf/mobile/purple/">더보기</a>
+        </span>
+      </div>
+      </div>
+    </div>
   </h1>
 </section>
+
+
 
 <?php
 require_once "foot.php";
